316c936afdcd92604332802dbf8831acc042d85e
Your Name <you@example.com>
2022-03-29 13:02:53 +1000

    logo change

--- src/components/icons/RenIcons.tsx
+++ src/components/icons/RenIcons.tsx
@@ -118,7 +118,7 @@ export const SpecialAlertIcon: CustomSvgIconComponent = (props) => (
 );
 
 export const BridgeLogoIcon: CustomSvgIconComponent = (props) => {
-  const scalingProps = getScalingPropsFactored(850.39, 350.39, 3.2);
+  const scalingProps = getScalingPropsFactored(1000, 1000, 8.2);
   return<SvgIcon fill="black" component={BridgeLogo} {...scalingProps} {...props}/>
 };
 
